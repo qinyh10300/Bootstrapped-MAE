96cbe2701eb766d174f665d806aae1898ee20065
Yihua Qin <qinyh10300@gmail.com>
2025-04-23 08:56:46 +0800

    modify save_checkpoint

--- main_pretrain.py
+++ main_pretrain.py
@@ -197,7 +197,7 @@ def main(args):
         if args.output_dir and (epoch % 20 == 0 or epoch + 1 == args.epochs):
             misc.save_model(
                 args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
-                loss_scaler=loss_scaler, epoch=epoch)
+                loss_scaler=loss_scaler, epoch=epoch, checkpoint_name=args.model)
 
         log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
                         'epoch': epoch,}
