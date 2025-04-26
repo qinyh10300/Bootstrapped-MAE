12fb312354d96a721ea94969a9a3a15108629b05
Yihua Qin <qinyh10300@gmail.com>
2025-04-26 16:55:02 +0800

    add mae_train.sh and modify mae_train.sh

--- bash_scripts/mae_train.sh
+++ bash_scripts/mae_train.sh
@@ -1,66 +1,24 @@
 #!/bin/bash
 export OMP_NUM_THREADS=4
 
-# 默认 YAML 配置文件路径
-DEFAULT_CONFIG_FILE="./experiments/default_mae_deit_tiny.yaml"
-
-# 解析命令行参数
-TEMP=$(getopt -o "c:" --long cfg: -n 'mae_train.sh' -- "$@")
-if [ $? != 0 ]; then
-    echo "Error parsing options."
-    exit 1
-fi
-
-# 设置解析后的参数
-eval set -- "$TEMP"
-
-# 初始化变量
-CONFIG_FILE=$DEFAULT_CONFIG_FILE
-
-# 处理命令行参数
-while true; do
-    case "$1" in
-        -c|--cfg)
-            CONFIG_FILE="$2"
-            shift 2
-            ;;
-        --)
-            shift
-            break
-            ;;
-        *)
-            echo "Unknown option: $1"
-            exit 1
-            ;;
-    esac
-done
-
-echo -e "\033[1;33mUsing config file: $CONFIG_FILE\033[0m"
-
-# 初始化变量，使用 YAML 文件中的默认值
-NUM_GPUS=$(yq -r '.NUM_GPUS' $CONFIG_FILE)
-MODEL=$(yq -r '.MODEL' $CONFIG_FILE)
-NAME=$(yq -r '.NAME' $CONFIG_FILE)
-
-echo -e "\033[1;33mModel: $NAME\033[0m"
-
-DATA_PATH=$(yq -r '.DATA_PATH' $CONFIG_FILE)
-OUTPUT_DIR=$(yq -r '.OUTPUT_DIR' $CONFIG_FILE)
-BATCH_SIZE=$(yq -r '.BATCH_SIZE' $CONFIG_FILE)
-ACCUM=$(yq -r '.ACCUM' $CONFIG_FILE)
-EPOCHS=$(yq -r '.EPOCHS' $CONFIG_FILE)
-WARMUP_EPOCHS=$(yq -r '.WARMUP_EPOCHS' $CONFIG_FILE)
-BASE_LR=$(yq -r '.BASE_LR' $CONFIG_FILE)
-INPUT_SIZE=$(yq -r '.INPUT_SIZE' $CONFIG_FILE)
-MASK_RATIO=$(yq -r '.MASK_RATIO' $CONFIG_FILE)
+NAME="mae_train_deit"
+MODEL="mae_deit_tiny_patch4"
+DATA_PATH="./dataset/cifar10_dataset"
+OUTPUT_DIR="./ckpts/${NAME}/pretrained"
+BATCH_SIZE=64
+ACCUM=2
+EPOCHS=200
+WARMUP_EPOCHS=10
+BASE_LR=1.5e-4
+INPUT_SIZE=32
+MASK_RATIO=0.75
 
 # 获取当前日期和时间
 CURRENT_DATETIME=$(date +"%Y-%m-%d_%H-%M-%S")
 
 # 动态生成日志目录
-LOG_DIR="./logs/mae_train/tb_${CURRENT_DATETIME}"
+LOG_DIR="./logs/${NAME}/tb_${NAME}_${CURRENT_DATETIME}"
 
-# 运行训练脚本
 python main_pretrain.py \
     --model ${MODEL} \
     --data_path ${DATA_PATH} \
@@ -73,4 +31,6 @@ python main_pretrain.py \
     --input_size ${INPUT_SIZE} \
     --mask_ratio ${MASK_RATIO} \
     --norm_pix_loss \
-    --log_dir ${LOG_DIR}
+    --log_dir ${LOG_DIR} \
+    --device cuda:2 \
+    --current_datetime ${CURRENT_DATETIME} \
--- main_pretrain.py
+++ main_pretrain.py
@@ -293,7 +293,7 @@ def main(args):
             if args.output_dir and (epoch % 20 == 0 or epoch + 1 == args.epochs):
                 misc.save_model(
                     args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
-                    loss_scaler=loss_scaler, epoch=epoch, checkpoint_name=args.model)
+                    loss_scaler=loss_scaler, epoch=epoch, checkpoint_name=f"mae_{args.current_datetime}")
 
             log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
                             'epoch': epoch,}
