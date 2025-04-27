21bda2d5805781a99c09d30aba13b5923339ed07
Yihua Qin <qinyh10300@gmail.com>
2025-04-28 00:38:45 +0800

    add pretrain_bootstrap_steps_tuning.py

--- experiments/pretrain_bootstrap_steps_tuning.py
+++ experiments/pretrain_bootstrap_steps_tuning.py
@@ -1,12 +1,13 @@
 import subprocess
 import json
 import os
+from datetime import datetime
 
 BOOTSTRAP_STEPS = [1, 2, 4, 8, 16, 32]
 USE_EMA = [True, False]
 
 # Output log file
-log_file = "./hyperparam_results/pretrain_bootstrap_steps_tuning.log"
+log_file = "./experiments/hyperparam_results/pretrain_bootstrap_steps_tuning.log"
 
 # Make sure to clear the log file before starting
 if os.path.exists(log_file):
@@ -15,12 +16,18 @@ if os.path.exists(log_file):
 # Function to run the training process and capture the last line from log.txt
 def run_training(bootstrap_steps, use_ema):
     # Define the output directory based on the hyperparameters
-    output_dir = f"../ckpts/mae_baseline/pretrain_bootstrap_steps_{bootstrap_steps}_use_ema_{use_ema}"
+    current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")  # 获取当前时间并格式化为字符串
+    print("current_datetime:", current_datetime)
+    name = "Bmae_deit_pretrain_bootstrap_steps_{bootstrap_steps}_use_ema_{use_ema}"
+    output_dir = f"./ckpts/{name}/{current_datetime}"
     
     # Run the training script using subprocess
     command = [
-        "bash", "Bmae_train.sh", 
+        "bash", "bash_scripts/Bmae_train.sh", 
         "--bootstrap_steps", str(bootstrap_steps),
+        "--current_datetime", str(current_datetime),
+        "--name", str(name),
+        "--device", "cuda:0",
     ]
 
     if use_ema:
@@ -51,6 +58,11 @@ def run_training(bootstrap_steps, use_ema):
             except json.JSONDecodeError:
                 print(f"Error parsing last line of {log_path}")
                 return None
+
+# 确保日志文件的父目录存在
+log_dir = os.path.dirname(log_file)
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
 
 with open(log_file, "a") as log:
     for bootstrap_steps in BOOTSTRAP_STEPS:
